4fb6b7a191589ff46a601498e3e24ab3701cfa54
Ivan Silva <is.ivancsilva@gmail.com>
2026-04-28 17:39:22 +0100

    fix: label secretario -> Secretário(a) departamental para distinguir do Geral

--- sitetibl/models.py
+++ sitetibl/models.py
@@ -225,7 +225,7 @@ class Mandato(models.Model):
         ('membro', 'Membro'),
         ('lider', 'Líder'),
         ('vice_lider', 'Vice-Líder'),
-        ('secretario', 'Secretário(a)'),
+        ('secretario', 'Secretário(a) departamental'),
         ('secretario_geral', 'Secretário(a) Geral'),
         ('tesoureiro', 'Tesoureiro(a)'),
         ('coordenador', 'Coordenador(a)'),
